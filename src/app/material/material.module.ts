fe3ca23881aa02028c307619a958dc0258158557
tz01x <abudr963rahman@gmail.com>
2022-10-01 21:36:19 +0600

    add basic layout in chat-view

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,8 +5,8 @@ import {MatIconModule} from  '@angular/material/icon'
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list'
 import { MatDividerModule } from '@angular/material/divider';
-
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
@@ -17,6 +17,8 @@ import { MatDividerModule } from '@angular/material/divider';
     MatButtonModule,
     MatListModule,
     MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
   ]
 })
 export class MaterialModule { }
