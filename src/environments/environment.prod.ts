81ffe9edaddbaf84fac0673c86f12ffb25eefa66
tz01x <abudr963rahman@gmail.com>
2022-10-15 22:08:00 +0600

    env added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  chatSocketUrl:'https://tumzied.onrender.com/afterNet',
+  api:'https://tumzied.pythonanywhere.com/afternet',
+  firebase : {
+    apiKey: "AIzaSyBb7AQNYrIPHZ9zu0yIr4zUjRKMnhIFgIE",
+    authDomain: "rt-chat-586d8.firebaseapp.com",
+    projectId: "rt-chat-586d8",
+    storageBucket: "rt-chat-586d8.appspot.com",
+    messagingSenderId: "285118792626",
+    appId: "1:285118792626:web:0babfcbae15ea1e06f5aab"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  chatSocketUrl:'https://tumzied.onrender.com/afterNet',
+  api:'https://tumzied.pythonanywhere.com/afternet',
+  firebase : {
+    apiKey: "AIzaSyBb7AQNYrIPHZ9zu0yIr4zUjRKMnhIFgIE",
+    authDomain: "rt-chat-586d8.firebaseapp.com",
+    projectId: "rt-chat-586d8",
+    storageBucket: "rt-chat-586d8.appspot.com",
+    messagingSenderId: "285118792626",
+    appId: "1:285118792626:web:0babfcbae15ea1e06f5aab"
+  }
 };
 
 /*
